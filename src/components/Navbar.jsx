a614ac1be84ffec7f55e9e58293b7651b80a7b81
Paul Airuehia <osasuair@gmail.com>
2023-08-03 21:27:04 -0400

    Design Changes/ Modifications to Home Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { Link } from 'react-scroll'
+import logo from '../images/logo512.png'
 
 const Navbar = () => {
 
@@ -31,13 +32,14 @@ const Navbar = () => {
 
     return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
-        <div>
-            <h1 className='text-5xl font-signature ml-2'>Paul Airuehia</h1>
+        <div className='flex flex-row items-center'>
+            <img src={logo} alt="Paul Airuehia" className='w-12 h-12'/>
+            <span className='hidden md:inline-block ml-2 text-2xl font-bold font-logoName text-teal-300 uppercase tracking-widest'>Paul<br/>Airuehia</span>
         </div>
 
         <ul className='hidden md:flex'>
             {links.map(({id, link}) => (
-                <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
+                <li key={id} className='px-4 cursor-pointer capitalize font-medium text-teal-500 hover:scale-105 duration-200'>
                     <Link to={link} smooth duration={500}>
                         {link}
                     </Link>
