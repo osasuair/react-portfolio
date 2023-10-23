e348a13fed3bc7ff356a838b1e73d521106db9e2
Paul Airuehia <osasuair@gmail.com>
2023-10-23 18:47:53 -0400

    Updates to resume details reflected in portfolio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,6 +14,35 @@ const Experience = () => {
             </div>
 
             <div className='h-full w-full grid sm:grid-cols-2 md:grid-cols-3 gap-5 mx-auto'>
+            <div>
+                <h2 className='font-extrabold text-xl lg:text-2xl bg-gradient-to-r text-transparent bg-clip-text from-teal-400 to-green-300 tracking-wide'>
+                  Experience - Computer Science
+                </h2>
+                {csExperience.map(({id, image, title, pos, desc, location, date}) => (
+                  <div key={id} className='rounded-xl my-5 hover:scale-105 duration-200 border-[1px] border-gray-600 bg-gray-800 bg-opacity-50'>
+                    <div className='min-h-[8rem] p-4 bg-gray-950 rounded-t-xl flex items-center justify-center'>
+                      <img className="mx-auto" src={image} alt={title}/>
+                    </div>
+                    <div className='p-4'>
+                      <ul className='py-2 leading-5'>
+                        <li className='text-xl font-bold' dangerouslySetInnerHTML={{__html: title}}></li>
+                        <li className='text-gray-400 font-bold mb-3'>{pos}</li>
+                        <div className='mb-3'>
+                            <ul className='list-disc list-inside'>
+                                {desc.map((course, index) => (
+                                    <li key={index} className='text-gray-300 text-sm mb-1'>{course}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <li className='text-gray-300'>{location}</li>
+                        <li className='text-gray-300 text-sm'>{date}</li>
+                      </ul>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+
               <div>
                 <div className='mb-5'>
                   <h2 className='font-bold text-teal-300 text-xl lg:text-2xl'>Education</h2>
@@ -47,34 +76,6 @@ const Experience = () => {
                     </div>
                   </div>
                 </div>
-              </div>
-
-              <div>
-                <h2 className='font-extrabold text-xl lg:text-2xl bg-gradient-to-r text-transparent bg-clip-text from-teal-400 to-green-300 tracking-wide'>
-                  Experience - Computer Science
-                </h2>
-                {csExperience.map(({id, image, title, pos, desc, location, date}) => (
-                  <div key={id} className='rounded-xl my-5 hover:scale-105 duration-200 border-[1px] border-gray-600 bg-gray-800 bg-opacity-50'>
-                    <div className='min-h-[8rem] p-4 bg-gray-950 rounded-t-xl flex items-center justify-center'>
-                      <img className="mx-auto" src={image} alt={title}/>
-                    </div>
-                    <div className='p-4'>
-                      <ul className='py-2 leading-5'>
-                        <li className='text-xl font-bold' dangerouslySetInnerHTML={{__html: title}}></li>
-                        <li className='text-gray-400 font-bold mb-3'>{pos}</li>
-                        <div className='mb-3'>
-                            <ul className='list-disc list-inside'>
-                                {desc.map((course, index) => (
-                                    <li key={index} className='text-gray-300 mb-1'>{course}</li>
-                                ))}
-                            </ul>
-                        </div>
-                        <li className='text-gray-300'>{location}</li>
-                        <li className='text-gray-300 text-sm'>{date}</li>
-                      </ul>
-                    </div>
-                  </div>
-                ))}
               </div>
 
               <div>
---
+++ src/data/Experience.js
@@ -0,0 +1,97 @@
+import ircc from '../images/experience/ircc.png'
+import carleton from '../images/experience/carleton.png'
+import canadianTire from '../images/experience/canadianTire.png'
+import swissKeg from '../images/experience/swissKeg.png'
+
+const education = [
+    {
+      id: 1,
+      title: 'Bachelor of Computer Science',
+      school: 'Carleton University',
+      desc: ["Stream/Specialization: Software Engineering"],
+      grade: 'GPA: 11.7/12.0',
+      date: 'Expected Graduation: May 2025'
+    },
+    {
+      id: 2,
+      title: 'Ontario Secondary School Diploma',
+      school: 'St. Roch Catholic Secondary School',
+      desc: ["Graduated with High Honours"],
+      date: 'Graduation: June 2021'
+    }
+]
+
+const awards = [
+    {
+        id: 1,
+        title: 'Carleton University Deans\' Honour List (2)',
+        date: 'May 2022, May 2023'
+    },
+    {
+        id: 2,
+        title: 'Walter A. Ainsworth Bursary ($1,900)',
+        date: 'January 2023'
+    },
+    {
+        id: 3,
+        title: 'Henry Marshall Tory Scholarship ($3,000)', 
+        date: 'September 2022'
+    },
+    {
+        id: 4,
+        title: 'Carleton University President\'s Scholarship ($12,000)',
+        date: 'September 2021'
+    }
+]
+
+const csExperience = [
+    {
+        id: 1,
+        image: ircc,
+        title: 'Test Automation Internship at <abbr title="Immigration, Refugees and Citizenship Canada">IRCC</abbr>',
+        pos: 'FSWEP Program',
+        desc: [
+        'Automated sanity and regression testing using Selenium and C#, reducing manual testing by 25%',
+        'Enhanced quality assurance by collaborating with various teams to integrate testing into CI/CD Pipeline via Azure DevOps',
+        'Identified and documented defects, improving detection of critical unidentified bugs',
+        'Contributed to enhancing the automation framework'
+        ],
+        location: 'Ottawa, Ontario / Remote',
+        date: 'May 2023 - Present'
+    },
+    {
+        id: 2,
+        image: carleton,
+        title: 'Teaching Assistant at Carleton University',
+        pos: 'Computer Science Dept.',
+        desc: [
+        'Communicated with faculty to enhance teaching techniques, developing modern approaches for students\' benefit',
+        'Marked assignments and exams in Python/Java, providing detailed feedback',
+        'Led multiple tutorial sessions of 30+ students, providing in-depth explanations',
+        'Mentored first-year students, boosting their confidence and motivation to learn'
+        ],
+        location: 'Ottawa, Ontario',
+        date: 'September 2022 - April 2023'
+    }
+]
+
+const otherExperience = [
+    {
+        id: 1,
+        image: canadianTire,
+        title: 'Warehouse Associate at Canadian Tire Corp.',
+        pos: 'Fast-Pick, Receiving, and Bulk Positions',
+        location: 'Bolton, Ontario',
+        date: 'June 2022 - August 2022'
+    },
+    {
+        id: 2,
+        image: swissKeg,
+        title: 'Kitchen Staff at Swiss Chalet and The Keg',
+        pos: 'Lead Line Cook, Kitchen Help, and Dishwasher',
+        location: 'Brampton, Ontario / Ottawa, Ontario',
+        date: 'November 2019 - April 2022'
+    }
+]
+
+export { education, awards, csExperience, otherExperience }
